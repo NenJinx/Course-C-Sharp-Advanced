407fed7292bba5f0fac612298023697e25550a97
NenJinx <mr.yordangeorgiev@gmail.com>
2023-05-13 18:02:35 +0300

    Add two tasks

---
+++ StacksAndQueues/FastFood/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+int foodQuantity = int.Parse(Console.ReadLine());
+List<int> orders = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+Queue<int> queue = new(orders);
+int theBiggestOrder = queue.Max();
+Console.WriteLine(theBiggestOrder);
+while (queue.Count > 0)
+{
+}
---
+++ StacksAndQueues/MaximumAndMinimum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int n = int.Parse(Console.ReadLine());
+
+Stack<int> stack = new();
+for (int i = 0; i < n; i++)
+{
+    int[] cmd = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse).ToArray();
+
+    if (cmd[0] == 1)
+    {
+        stack.Push(cmd[1]);
+    }
+    else if (cmd[0] == 2)
+    {
+        stack.Pop();
+    }
+    else if (cmd[0] == 3)
+    {
+        if (stack.Any())
+        {
+            Console.WriteLine(stack.Max());
+        }
+    }
+    else if (cmd[0] == 4)
+    {
+        if (stack.Any())
+        {
+            Console.WriteLine(stack.Min());
+        }
+    }
+}
+
+Console.WriteLine(string.Join(", ", stack));
