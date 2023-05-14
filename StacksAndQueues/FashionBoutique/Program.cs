c97dc6b7614ef87c0170dfba52260f070896ca3e
NenJinx <mr.yordangeorgiev@gmail.com>
2023-05-14 18:19:48 +0300

    Add

---
+++ StacksAndQueues/FashionBoutique/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Stack<int> boxWithClothes = new(Console.ReadLine().Split(' ').
+    Select(int.Parse));
+int theCapacityOfTheRack = int.Parse(Console.ReadLine());
+
+int theSumOfTheClothes = 0;
+int countRacks = 0;
+
+
+while (boxWithClothes.Any())
+{
+    if (theSumOfTheClothes + boxWithClothes.Peek() > theCapacityOfTheRack)
+    {
+        theSumOfTheClothes = 0;
+        countRacks++;
+        continue;
+    }
+    theSumOfTheClothes += boxWithClothes.Peek();
+    boxWithClothes.Pop();
+}
+countRacks++;
+Console.WriteLine(countRacks);
+
+
