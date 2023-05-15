c9c4da8b89503658c5e22bda5b6bf6b4586e6b48
NenJinx <mr.yordangeorgiev@gmail.com>
2023-05-15 22:18:34 +0300

    Add two

---
+++ StacksAndQueues/SongsQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿Queue<string> songs = new(Console.ReadLine().Split(", "));
+string[] cmdArgs = new string[2];
+while (songs.Any())
+{
+    
+    cmdArgs = Console.ReadLine().Split(' ');
+    string command = cmdArgs[0];
+    if (command =="Play")
+    {
+        songs.Dequeue();
+    }
+    else if (command == "Add")
+    {
+        string song = string.Join(" ",cmdArgs[1..]);
+        if (songs.Contains(song))
+        {
+            Console.WriteLine($"{song} is already contained!");
+        }
+        else
+        {
+            songs.Enqueue(song);
+        }
+    }
+    else
+    {
+        Console.WriteLine(string.Join(", ",songs));
+    }
+}
+Console.WriteLine("No more songs!");
