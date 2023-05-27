bab813d361cf1d438b575df7317ca27be741798c
NenJinx <mr.yordangeorgiev@gmail.com>
2023-05-27 17:57:41 +0300

    Added

---
+++ StreamFiles/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,26 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using FileStream file = new(inputFilePath, FileMode.Open);
+            {
+                FileStream newFile = new(outputFilePath, FileMode.Create);
+                file.CopyTo(newFile);
+            }
+
+        }
+    }
+}
