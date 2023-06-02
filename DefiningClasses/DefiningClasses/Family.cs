fc68d7d3917fef4fc796a5633c15e73427947f2d
Yordan <mr.yordangeorgiev@gmail.com>
2023-06-02 20:25:41 +0300

    Added

---
+++ DefiningClasses/DefiningClasses/Family.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    internal class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            people = new List<Person>();
+        }
+        public void AddMember(Person person)
+        {
+            people.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.people.MaxBy(p => p.Age);
+        }
+    }
+}
---
+++ DefiningClasses/DefiningClasses/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<Person> peopleOver30 = new();
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string[] personData = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            Person person = new(personData[0], int.Parse(personData[1]));
+            if (person.Age > 30)
+            {
+                peopleOver30.Add(person);
+            }
+        }
+        foreach (var item in peopleOver30)
+        {
+            Console.WriteLine($"{item.Name} {item.Age}");
+        }
+        
+        
+    }
+}
