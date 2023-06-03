9a55da7c826835aa08c0efba569852e5c6f01442
NenJinx <122741133+NenJinx@users.noreply.github.com>
2023-06-03 14:21:46 +0300

    Added a task

--- StacksAndQueues/BasicStackOperation/Program.cs
+++ StacksAndQueues/BasicStackOperation/Program.cs
@@ -10,7 +10,7 @@ namespace BasicStackOperation
     {
         static void Main(string[] args)
         {
-            int[] cmdArgs = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] cmdArgs = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
             int countElementsForPush = cmdArgs[0];
             int elementsForPop = cmdArgs[1];
             int elementForSearching = cmdArgs[2];
