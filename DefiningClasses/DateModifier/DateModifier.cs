8ff24cf54f5969400ee6bb4185e44b9eb2e345c6
Yordan <mr.yordangeorgiev@gmail.com>
2023-06-06 17:40:28 +0300

    Defining Classes solutions

---
+++ DefiningClasses/DateModifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace DateModifier
+{
+    public static class DateModifier
+    {
+        public static int CalculateDifference(string firstDateData, string secondDateData)
+        {
+            DateTime firstDate = DateTime.Parse(firstDateData);
+            DateTime secondDate = DateTime.Parse(secondDateData);
+            TimeSpan difference = firstDate - secondDate;
+            return Math.Abs(difference.Days);
+        }
+       
+    }
+}
+
---
+++ DefiningClasses/DateModifier/StartProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateModifier
+{
+    public class StartProgram
+    {
+        static void Main(string[] args)
+        {
+            string firstDateData = Console.ReadLine();
+            string secondDateData = Console.ReadLine();
+            int difference = DateModifier.CalculateDifference(firstDateData, secondDateData);
+            Console.WriteLine(difference);
+        }
+    }
+}
