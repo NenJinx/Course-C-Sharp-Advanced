3d367cfd4b124308b885a7ddf0b8129a3454df97
Yordan <mr.yordangeorgiev@gmail.com>
2023-06-06 17:34:38 +0300

    Defining Classes solutions

---
+++ DefiningClasses/RawData/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawData
+{
+    public class Car
+    {
+        public Car(string model, int engineSpeed, int enginePower, int cargoWeight,string cargoType, double tire1Pressure,
+            int tire1Age, double tire2Pressure, int tire2Age, double tire3Pressure,int  tire3Age,double tire4Pressure, 
+            int tire4Age)
+        {
+            Model = model;
+            Engine = new(engineSpeed, enginePower);
+            Cargo = new(cargoWeight, cargoType);
+            Tires = new Tire[4];
+            Tires[0] = new(tire1Pressure, tire1Age);
+            Tires[1] = new(tire2Pressure, tire2Age);
+            Tires[2] = new(tire3Pressure, tire3Age);
+            Tires[3] = new(tire4Pressure, tire4Age);        
+        }
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tire[] Tires { get; set; }
+    }
+}
---
+++ DefiningClasses/RawData/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Car car = new(
+                    cmdArgs[0],
+                    int.Parse(cmdArgs[1]),
+                    int.Parse(cmdArgs[2]),
+                    int.Parse(cmdArgs[3]),
+                    cmdArgs[4],
+                    double.Parse(cmdArgs[5]),
+                    int.Parse(cmdArgs[6]),
+                    double.Parse(cmdArgs[7]),
+                    int.Parse(cmdArgs[8]),
+                    double.Parse(cmdArgs[9]),
+                    int.Parse(cmdArgs[10]),
+                    double.Parse(cmdArgs[11]),
+                    int.Parse(cmdArgs[12]));
+
+                cars.Add(car);
+            }
+            string command = Console.ReadLine();
+            string[] filteredCar;
+            if (command == "fragile")
+            {                    
+                filteredCar = cars.Where(c => c.Cargo.Type == "fragile" && c.Tires.Any(t => t.Pressure < 1)).
+                    Select(c => c.Model)
+                    .ToArray();
+            }
+            else
+            {
+                filteredCar = cars.Where(c => c.Cargo.Type == "flammable" && c.Engine.Power > 250).
+                    Select(c => c.Model)
+                    .ToArray();
+            }
+            foreach (var car in filteredCar) 
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
