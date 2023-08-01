e65fd4074dcb7965a0edaf2e5857693c26ec3530
NenJinx <122741133+NenJinx@users.noreply.github.com>
2023-08-01 14:07:11 +0300

    Sets and Dictionaries Advanced solutions

--- SetsAndDictionriesAdvanced/SetsOfElements/Program.cs
+++ SetsAndDictionriesAdvanced/SetsOfElements/Program.cs
@@ -3,6 +3,6 @@ HashSet<string> uniqueUsernames = new HashSet<string>();
 for (int i = 0; i < n; i++)
 {
     string command = Console.ReadLine();
-    uniqueUsernames.Add(command);
+    uniqueUsernames.Add(command); 
 }
 Console.WriteLine(string.Join("\n", uniqueUsernames));
