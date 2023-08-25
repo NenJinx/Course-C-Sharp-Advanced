418ae4ecb400c9e1c174df2db271c9860869a793
NenJinx <122741133+NenJinx@users.noreply.github.com>
2023-08-25 19:46:45 +0300

    Stacks and Queues solutions

--- StacksAndQueues/FastFood/Program.cs
+++ StacksAndQueues/FastFood/Program.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 int foodQuantity = int.Parse(Console.ReadLine());
-List<int> orders = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-Queue<int> queue = new(orders);
-int theBiggestOrder = queue.Max();
-Console.WriteLine(theBiggestOrder);
-while (queue.Count > 0)
+Queue<int> orders = new(Console.ReadLine().Split(' ').Select(int.Parse));
+Console.WriteLine(orders.Max());
+while (orders.Any())
 {
+    foodQuantity -= orders.Peek();
+    if (foodQuantity < 0)
+    {
+        break;
+    }
+    orders.Dequeue();
+}
+if (orders.Any())
+{
+    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+}
+else
+{
+    Console.WriteLine("Orders complete");
 }
