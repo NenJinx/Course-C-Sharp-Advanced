f3ca2a5189e05a8945b2ff5cd2536ff72154ae12
Yordan <mr.yordangeorgiev@gmail.com>
2023-09-06 15:35:27 +0300

    a

--- StreamsFilesAndDirectories/EvenLines/EvenLines.cs
+++ StreamsFilesAndDirectories/EvenLines/EvenLines.cs
@@ -1,52 +1,78 @@
-﻿namespace EvenLines
+﻿//namespace EvenLines
+//{
+//    using System;
+//    using System.IO;
+//    using System.Linq;
+//    using System.Text;
+
+//    public class EvenLines
+//    {
+//        static void Main()
+//        {
+//            string inputFilePath = @"..\..\..\text.txt";
+
+//            Console.WriteLine(ProcessLines(inputFilePath));
+//        }
+
+//        public static string ProcessLines(string inputFilePath)
+//        {
+//            using StreamReader inputStreamReader = new StreamReader(inputFilePath);
+//            int count = 0;
+//            StringBuilder modifiedText = new();
+//            string line;
+//            while ((line = inputStreamReader.ReadLine()) != null)
+//            {
+//                if (count % 2 == 0)
+//                {
+//                    string replacedSymbols = ReplaceSymbols(line);
+//                    string reversedLine = ReverseText(replacedSymbols);
+//                    modifiedText.AppendLine(reversedLine);
+//                }
+//                count++;
+//            }
+//            return modifiedText.ToString();
+//        }
+//        private static string ReplaceSymbols(string text)
+//        {
+//            StringBuilder sb = new(text);
+//            char[] symbolsToReplace = { '-', ',', '.', '!', '?' };
+//            foreach (var symbol in symbolsToReplace)
+//            {
+//                sb.Replace(symbol, '@');
+//            }
+//            return sb.ToString();
+//        }
+//        private static string ReverseText(string words)
+//        {
+//            string[] reversedWords = words.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+//                .Reverse().ToArray();
+//            return string.Join(" ", reversedWords);
+//        }
+//    }
+//}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int foodQuantity = int.Parse(Console.ReadLine());
+Queue<int> orders = new(Console.ReadLine().Split(' ').Select(int.Parse));
+Console.WriteLine(orders.Max());
+while (orders.Any())
 {
-    using System;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-
-    public class EvenLines
+    foodQuantity -= orders.Peek();
+    if (foodQuantity < 0)
     {
-        static void Main()
-        {
-            string inputFilePath = @"..\..\..\text.txt";
-
-            Console.WriteLine(ProcessLines(inputFilePath));
-        }
-
-        public static string ProcessLines(string inputFilePath)
-        {
-            using StreamReader inputStreamReader = new StreamReader(inputFilePath);
-            int count = 0;
-            StringBuilder modifiedText = new();
-            string line;
-            while ((line = inputStreamReader.ReadLine()) != null)
-            {
-                if (count % 2 == 0)
-                {
-                    string replacedSymbols = ReplaceSymbols(line);
-                    string reversedLine = ReverseText(replacedSymbols);
-                    modifiedText.AppendLine(reversedLine);
-                }
-                count++;
-            }
-            return modifiedText.ToString();
-        }
-        private static string ReplaceSymbols(string text)
-        {
-            StringBuilder sb = new(text);
-            char[] symbolsToReplace = { '-', ',', '.', '!', '?' };
-            foreach (var symbol in symbolsToReplace)
-            {
-                sb.Replace(symbol, '@');
-            }
-            return sb.ToString();
-        }
-        private static string ReverseText(string words)
-        {
-            string[] reversedWords = words.Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .Reverse().ToArray();
-            return string.Join(" ", reversedWords);
-        }
+        break;
     }
+    orders.Dequeue();
 }
+if (orders.Any())
+{
+    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+}
+else
+{
+    Console.WriteLine("Orders complete");
+}
+
--- StreamsFilesAndDirectories/LineNumbers/LineNumbers.cs
+++ StreamsFilesAndDirectories/LineNumbers/LineNumbers.cs
@@ -1,29 +1,67 @@
-﻿namespace LineNumbers
-{
-    using System;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
+﻿//namespace LineNumbers
+//{
+//    using System.IO;
+//    using System.Linq;
+//    using System.Text;
+//    public class LineNumbers
+//    {
+//        static void Main()
+//        {
+//            string inputFilePath = @"..\..\..\text.txt";
+//            string outputFilePath = @"..\..\..\output.txt";
+//            ProcessLines(inputFilePath, outputFilePath);
+//        }
+//        public static void ProcessLines(string inputFilePath, string outputFilePath)
+//        {
+//            StringBuilder modifiedText = new();
+//            string[] inputFile = File.ReadAllLines(inputFilePath);
+//            for (int i = 0; i < inputFile.Length; i++)
+//            {
+//                int countLetters = inputFile[i].Count(char.IsLetter);
+//                int countSymbols = inputFile[i].Count(char.IsPunctuation);
+//                modifiedText.AppendLine($"{i + 1}: {inputFile[i]} ({countLetters})({countSymbols})");
+//            }
+//            File.WriteAllText(outputFilePath, modifiedText.ToString());
+//        }
+//    }
+//}
+using System;
+using System.Collections.Generic;
 
-    public class LineNumbers
+
+namespace CompanyUsers
+{
+    internal class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            string inputFilePath = @"..\..\..\text.txt";
-            string outputFilePath = @"..\..\..\output.txt";
-            ProcessLines(inputFilePath, outputFilePath);
-        }
-        public static void ProcessLines(string inputFilePath, string outputFilePath)
-        {
-            StringBuilder modifiedText = new();
-            string[] inputFile = File.ReadAllLines(inputFilePath);
-            for (int i = 0; i < inputFile.Length; i++)
+            Dictionary<string, List<string>> companiesAndWorkers = new Dictionary<string, List<string>>();
+            String[] separator = { " -> " };
+            string command;
+            while ((command = Console.ReadLine()) != "End")
             {
-                int countLetters = inputFile[i].Count(char.IsLetter);
-                int countSymbols = inputFile[i].Count(char.IsPunctuation);
-                modifiedText.AppendLine($"{i + 1}: {inputFile[i]} ({countLetters})({countSymbols})");
+                string[] cmdArgs = command.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                string companyName = cmdArgs[0];
+                string employeeID = cmdArgs[1];
+
+                if (!companiesAndWorkers.ContainsKey(companyName))
+                {
+                    companiesAndWorkers[companyName] = new List<string>();
+                }
+                if (companiesAndWorkers[companyName].Contains(employeeID))
+                {
+                    continue;
+                }
+                companiesAndWorkers[companyName].Add(employeeID);
             }
-            File.WriteAllText(outputFilePath, modifiedText.ToString());
+            foreach (var companyName in companiesAndWorkers)
+            {
+                Console.WriteLine($"{companyName.Key}");
+                foreach (var employeeID in companyName.Value)
+                {
+                    Console.WriteLine($"-- {employeeID}");
+                }
+            }
         }
     }
 }
